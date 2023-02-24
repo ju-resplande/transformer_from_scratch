514475722f5acf4075ab6c3b6904dde430fcf7e7
ju-resplande <julianarsg13@gmail.com>
2023-02-24 07:34:24 -0300

    self attention done

---
+++ TODO.md
@@ -0,0 +1,5 @@
+1. Setup.py
+2. Readme
+3. Atualizar nlp studies
+4. Testar
+5. Fazer colab
--- src/Transformer/self_attention.py
+++ src/Transformer/self_attention.py
@@ -26,9 +26,24 @@ class SelfAttention(nn.Module):
             keys = values.reshape(batch_dim, keys_len, self.heads, self.head_dim)
             queries = values.reshape(batch_dim, query_len, self.heads, self.head_dim)
 
-            # queries shape: (batch_dim, query_len, heads, key_len)
-            # keys shape: (batch_dim, key_len, heads, key_len)
+            # queries shape: (batch_dim, query_len, heads, heads_dim)
+            # keys shape: (batch_dim, key_len, heads, heads_dim)
             # energy shape: (batch_dim, heads, query_len, key_len)
             energy = torch.eisum("bqhd,bk->bh", [queries, keys])
 
-            if 
+            if mask == None:
+                # if the element of the mask equals 0
+                energy = energy.masked_fill(mask == 0, float('-inf'))
+            
+            #Softmax normalized in key_len (input/output)
+            attention = torch.softmax(energy / (self.embed_size) ** (1/2), dim=3)
+
+            # attention shape: (batch_dim, heads, query_len, key_len)
+            # values shape: (batch_dim, value_len, heads, heads_dim)
+            # (batch_dim, query_len, heads, head_dim)
+            out = torch.eisum("bhql,blhd->bqhd", [attention, values])
+            out = out.reshape(self.batch_dim, query_len, self.embed_size)
+
+            out = self.fc(out)
+
+            return out
