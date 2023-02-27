9f9ee04d68f680a9287b24e1390ab68847129276
ju-resplande <julianarsg13@gmail.com>
2023-02-27 15:22:02 -0300

    decoder

--- src/Transformer/decoder.py
+++ src/Transformer/decoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from self_attention import SelfAttention
-from transformer import TransformerBlock
+from transformer_block import TransformerBlock
 
 class DecoderBlock(nn.Module):
     def __init__(
@@ -14,11 +14,54 @@ class DecoderBlock(nn.Module):
         device
     ):
 
-    super(DecoderBlock, self).__init__()
-    self.attention = SelfAttention(embed_size, heads)
-    self.norm = nn.LayerNorm(embed_size)
-    self.transformer_block = TransformerBlock(
-        embed_size, heads, dropout, forward_expansion
-    )
+        super(DecoderBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformer_block = TransformerBlock(
+            embed_size, heads, dropout, forward_expansion
+        )
 
-    self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, src_mask, trg_mask):
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm(attention +x))
+        out self.transformer_block(value, key, query, src_mask)
+        return out
+    
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        trg_vocab_size,
+        embed_size,
+        num_layers,
+        heads,
+        forward_expansion,
+        dropout,
+        device,
+        max_length
+    ):
+        super(Decoder, self).__init__()
+        self.device = device
+        self.word_embedding = nn.Embedding(trg_vocab_size, embed_size)
+        self.position_embeeding = nn.Embedding(max_length, embed_size)
+
+        self.layers = ModuleList(
+            [DecoderBlock(embed_size, heads, forward_expansion, dropout, device)
+                for _ in range(num_layers)]
+        )
+
+        self.fc = nn.Linear(embed_size, trg_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_out, src_mask, trg_mask):
+        N, seq_length = x.shape
+        positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
+        x = self.dropout(self.word_embedding(x) + self.position_embeding(positions))
+
+        for layer in self.layers:
+            x = layer(x, enc_out, enc_out, src_mask, trg_mask)
+
+        out = self.fc(x)
+
+        
